b7cc4174547caab82868fdd6b1d1fbb6aa30bf28
Menno van Grinsven <scanno71@gmail.com>
2016-07-03 11:37:09 +0200

    clark: Add cryptfs_hw to device tree and enable TARGET_HW_DISK_ENCRYPTION
    Change-Id: I1dd46939159b0fe9b957def318c7ffa1b9f474b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,8 @@ BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw
 
 # Crypto
-TARGET_HW_DISK_ENCRYPTION := false
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
 
 # CPU
 ENABLE_CPUSETS := true
